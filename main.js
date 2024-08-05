d3d519839fb408ad1768aa50eee591559236d2f1
girishyadav <girishayadav.com>
2024-08-05 18:15:57 +0530

    second Change

--- main.js
+++ main.js
@@ -9,11 +9,7 @@ function add(num1, num2) {
 // Function to perform subtraction
 function subtract(num1, num2) {
   // TODO: Perform subtraction operation here
-  if (num1 > num2) {
-    return num1 - num2;
-  } else {
-    return num2 - num1;
-  }
+  return num1 - num2;
 }
 
 // Function to perform multiplication
@@ -33,10 +29,26 @@ function divide(num1, num2) {
 }
 
 // Main program
+// function calculator(num1, num2, operation) {
+//   // TODO: Get user input for numbers and operation
+//   // TODO: Call the appropriate function based on the operation
+//   // TODO: Output the result
+//   switch (operation) {
+//     case '+':
+//       return add(num1, num2);
+//     case '-':
+//       return subtract(num1, num2);
+//     case '*':
+//       return multiply(num1, num2);
+//     case '/':
+//       return divide(num1, num2);
+//     default:
+//       throw new Error("Invalid operation"); 
+//   }
+// }
 function calculator(num1, num2, operation) {
-  // TODO: Get user input for numbers and operation
-  // TODO: Call the appropriate function based on the operation
-  // TODO: Output the result
+  num1 = parseFloat(num1);
+  num2 = parseFloat(num2);
   switch (operation) {
     case '+':
       return add(num1, num2);
